9c3dae4eaf36589669790caf94ca69c5aa78585b
José de Jesus Filho <jjesusfilho@gmail.com>
2018-03-08 00:19:45 +0000

    First commit

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "gaston.stat@gmail.com"
-git config --global user.name "Gaston Sanchez"
+git config --global user.email "jjesusfilho@gmail.com"
+git config --global user.name "José de Jesus Filho"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
